cdb2c49a38ca743b87e8ba13fbbb18df886b3d6f
Luca Peric <lucaperic.lp@gmail.com>
2023-07-24 22:30:45 +0100

    Adding explicit **/*.md inclusion rule for remote copy

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ mod rclone;
 
 fn main() {
     rclone::defaults::start();
-    let result = rclone::defaults::list().unwrap();
-    // let result = rclone::defaults::copy().unwrap();
-    println!("{}", result);
+    let _ = rclone::defaults::copy().unwrap();
+    println!("Completed!")
 }
--- src/rclone/defaults.rs
+++ src/rclone/defaults.rs
@@ -24,13 +24,18 @@ pub fn list() -> Result<String, String>{
 
 pub fn copy() -> Result<String, String>{
     let method = String::from("sync/copy");
-    let tmp_dir = tempfile::tempdir();
+    let tmp_dir = tempfile::tempdir().unwrap(); // idgaf unwrap
+    let tmp_dir_path = tmp_dir.path().to_str().unwrap();
     let input = serde_json::json!(
         {
-            "srcFs": "lp-lucaperic:",
-            "dstFs": "knowledge-garden"
+            "srcFs": "lp-lucaperic:knowledge-garden",
+            "dstFs": tmp_dir_path,
+            "_filter": {
+                "IncludeRule": ["**/*.md", "*.md"],
+            }
         }
     );
+    println!("{}", tmp_dir_path);
     run(&method, &input.to_string())
 }
 
