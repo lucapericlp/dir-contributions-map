3ef04a8a205e38394f7436e2103a4854c33dd9ea
Luca Peric <lucaperic.lp@gmail.com>
2023-09-15 22:56:00 +0100

    Adding the created & modified check to the walker.filter_entry() call

--- src/collect.rs
+++ src/collect.rs
@@ -4,22 +4,52 @@ use crate::state::{State, DateMetadata};
 use chrono::{NaiveDate, DateTime, Utc};
 use walkdir;
 
-fn is_relevant(entry: &walkdir::DirEntry, file_ext: &str) -> bool {
+fn is_relevant(
+    entry: &walkdir::DirEntry, file_ext: &str, recency: chrono::Duration
+) -> bool {
     println!("Checking {}...", entry.file_name().to_str().unwrap());
     if entry.file_type().is_dir(){
         return true;
-    } else {
-        return entry.file_name()
-            .to_str()
-            .map(|s| s.ends_with(&file_ext))
-            .unwrap_or(false)
     }
+
+    let is_markdown_file = entry.file_name()
+        .to_str()
+        .map(|s| s.ends_with(&file_ext))
+        .unwrap_or(false);
+
+    if !is_markdown_file {
+        return false;
+    }
+
+    match entry.metadata() {
+        Ok(metadata) => {
+            if let Ok(created_time) = metadata.created() {
+                let elapsed_since = chrono::Duration::from_std(created_time.elapsed().unwrap());
+                if elapsed_since.unwrap() <= recency {
+                    return true;
+                    // still check if it was modified in this window
+                }
+            }
+            if let Ok(modified_time) = metadata.modified() {
+                let elapsed_since = chrono::Duration::from_std(modified_time.elapsed().unwrap());
+                return elapsed_since.unwrap() <= recency;
+            }
+        }
+        Err(e) => {
+            eprintln!("Failed to get metadata for file {}: {}", entry.path().display(), e);
+            return false;
+        }
+    }
+
+    false
 }
 
-pub fn add_recent_stats(path: &str, state: &State) -> Result<(), ()>{
+pub fn add_recent_stats(
+    path: &str, state: &mut State, recency: chrono::Duration
+) -> Result<(), ()>{
     let file_ext = ".md";
     let walker = walkdir::WalkDir::new(path).into_iter();
-    for entry in walker.filter_entry(|e| is_relevant(e, file_ext)){
+    for entry in walker.filter_entry(|e| is_relevant(e, file_ext, recency)){
         let _entry = entry.as_ref().unwrap();
         match _entry.file_type().is_dir() {
             true => continue,
@@ -28,17 +58,16 @@ pub fn add_recent_stats(path: &str, state: &State) -> Result<(), ()>{
                 let md = fs::metadata(entry_path).unwrap();
                 let dt_created: DateTime<Utc> = md.created().unwrap().into();
                 let dtn_created = dt_created.date_naive();
-                if state.entries.contains_key(&dtn_created){
-                     // test
-                }
-                md.modified();
-                state.entries.insert(key, value);
+                let last_modified = md.modified();
                 println!("{} last modified on {:?}", entry_path.display(), last_modified)
+                // if state.entries.contains_key(&dtn_created){
+                //      // test
+                //     state.entries.get(&dtn_created);
+                // } else {
+                //     // new
+                // }
             }
         }
     }
-    DateMetadata{
-        updates: 0,
-        creations: 0,
-    }
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use chrono;
+
 mod rclone;
 mod state;
 mod collect;
@@ -5,18 +7,21 @@ mod collect;
 fn main() {
     let user_state_file = "./state_file.json";
     let source = "lp-lucaperic:knowledge-garden/node_modules/";
+    let consideration_window = chrono::Duration::days(1);
 
     let state_file = state::determine_statefile(user_state_file.to_string());
+    let mut entire_state: state::State;
     if state_file.exists() {
-        let state = state_file.load_state();
+        entire_state = state_file.load_state();
     } else {
         state_file.touch().unwrap();
-        let state: state::State = Default::default();
+        entire_state = Default::default();
     }
 
     rclone::defaults::start();
-    let tmp_dir = tempfile::tempdir().unwrap()
+    let rclone_dir = tempfile::tempdir().unwrap()
         .path().to_str().unwrap().to_string();
-    rclone::defaults::copy(source, &tmp_dir).unwrap();
-    let day_stats = collect::get_recent_stats(&tmp_dir);
+    rclone::defaults::copy(source, &rclone_dir).unwrap();
+    // mutate entire state cos wgaf?
+    let day_stats = collect::add_recent_stats(&rclone_dir, &mut entire_state, consideration_window);
 }
